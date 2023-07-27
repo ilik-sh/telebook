caf341f9724cb82067e704cb3593f901f86329ae
Ilya <i.shyshparonak@gmail.com>
2023-07-27 15:16:14 +0300

    Update Employee and Unit models

--- telebook-back/models/models.js
+++ telebook-back/models/models.js
@@ -4,24 +4,23 @@ const { DataTypes } = require('sequelize')
 const Employee = sequelize.define('employee', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, allowNull: false},
+    pasport: {type: DataTypes.STRING, allowNull: false},
     email: {type: DataTypes.STRING, unique: true},
-    phone: {type: DataTypes.STRING, allowNull: false}
-})
+    phone1: {type: DataTypes.STRING, allowNull: true},
+    phone2: {type: DataTypes.STRING, allowNull: true}
+}, {createdAt: false, updatedAt: false })
 
 const Unit = sequelize.define('unit', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, allowNull: false}
-})
+    name: {type: DataTypes.STRING, allowNull: false},
+    weight: {type: DataTypes.INTEGER},
+}, {createdAt: false, updatedAt: false })
 
 const Position = sequelize.define('position', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     weight: {type: DataTypes.INTEGER},
-    name: {type: DataTypes.STRING, allowNull: false}
-})
-
-const UnitHierarchy = sequelize.define('unitHierarchy', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
-})
+    name: {type: DataTypes.STRING, allowNull: false},
+}, {createdAt: false, updatedAt: false })
 
 Unit.hasMany(Employee)
 Employee.belongsTo(Unit)
@@ -29,14 +28,10 @@ Employee.belongsTo(Unit)
 Position.hasMany(Employee)
 Employee.belongsTo(Position)
 
-UnitHierarchy.belongsTo(Unit, {as: 'parent'})
-UnitHierarchy.belongsTo(Unit, {as: 'child'})
-
-
+Unit.belongsTo(Unit, {as: 'parent', allowNull: true})
 
 module.exports = { 
     Employee, 
     Unit, 
-    Position, 
-    UnitHierarchy
+    Position
 }
